20d9bfb733261800fa1138c24b0e2f1e6d6822a5
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2022-02-11 22:09:05 -0500

    Filter and sort functions

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+
 
 class Crawler:
     """    
@@ -7,49 +9,89 @@ class Crawler:
     to scrap the title, order number, comments,
     and points of the first 30 entries.
     """
+
     def __init__(self, url="https://news.ycombinator.com/"):
         self.url = url
         self._response = requests.get(url)
-        self._soup = BeautifulSoup(self._response.content, 'lxml')    
-    
+        self._soup = BeautifulSoup(self._response.content, 'lxml')
+
     def _crawl(self, tag, items_list):
         for item in self._soup.find_all('tr'):
             if item.select(tag):
-                entrie = item.select(tag)[0].get_text()
-                items_list.append(entrie)
+                if len(item.select(tag)) == 1:
+                    entrie = item.select(tag)[0].get_text()
+                    try:
+                        if tag == '.score':
+                            entrie = entrie.split()[0]
+                            entrie = int(entrie)
+                        elif tag == '.rank':
+                            entrie = entrie.replace('.', '').strip()
+                            entrie = int(entrie)
+                    except ValueError:
+                        entrie = 0
+                    items_list.append(entrie)
         return items_list
-    
-    def crawl_order_number(self):
+
+    def _crawl_order_number(self):
         order_numbers = []
-        return self._crawl('.rank', order_numbers)    
+        return self._crawl('.rank', order_numbers)
 
-
-    def crawl_title(self):
+    def _crawl_title(self):
         news_titles = []
         return self._crawl('.titlelink', news_titles)
 
-    def crawl_points(self):
+    def _crawl_points(self):
         news_points = []
         return self._crawl('.score', news_points)
 
-
-    def crawl_points(self):
+    def _crawl_comments(self):
         news_comments = []
         for item in self._soup.find_all('tr'):
             if item.select('.score'):
-                comments = item.find_all('a')[3].get_text()
-                news_commentst.append(comments)
+                if len(item.select('.score')) == 1:
+                    comments = item.find_all('a')[3].get_text()
+                    comments = comments.split()[0]
+                    try:
+                        comments = int(comments)
+                    except ValueError:
+                        comments = 0
+                    news_comments.append(comments)
+
         return news_comments
 
+    def get_entries(self):
+        news_orders = self._crawl_order_number()
+        news_titles = self._crawl_title()
+        news_points = self._crawl_points()
+        news_comments = self._crawl_comments()
+        entries_df = pd.DataFrame(
+            {
+                "Order": [order for order in news_orders],
+                "Title": [title for title in news_titles],
+                "Points": [point for point in news_points + [0]],
+                "Comments": [int(comment) for comment in news_comments + [0]],
+            }
+        )
+        return entries_df
 
+    def filter_more_five_words(self, df):
+        sorted_df = df.sort_values('Comments', ascending=True)
+        count_words = sorted_df['Title'].str.split().str.len()
+        five_words_df = sorted_df[count_words > 5]
+        return five_words_df
+
+    def filter_less_five_words(self, df):
+        sorted_df = df.sort_values('Points', ascending=True)
+        count_words = sorted_df['Title'].str.split().str.len()
+        five_words_df = sorted_df[count_words <= 5]
+        return five_words_df
 
 
 if __name__ == '__main__':
-    news = {}
     crawler = Crawler()
-    orders_numbers = crawler.crawl_order_number()
-    news_titles = crawler.crawl_title()
-    for (order_number, new_title) in zip(orders_numbers, news_titles):
-        news[order_number] = new_title
-    print(news)
-    
+    entries_df = crawler .get_entries()
+    print(crawler.get_entries())
+    print("-----More than five words-------\n")
+    print(crawler.filter_more_five_words(entries_df), "\n")
+    print("-----Less equals than five words-------\n")
+    print(crawler.filter_less_five_words(entries_df), "\n")
